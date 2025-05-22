5a0ed80d14ed38ed4a2055eea31395eb6a9dc282
Chad Beard <cabeard21@gmail.com>
2025-05-22 13:18:29 -0500

    More debugging

--- demo/index.py
+++ demo/index.py
@@ -32,7 +32,8 @@ async def get_fight_data(
     update_status(f"Fetched {len(id_list)} IDs, requesting bulk data...")
 
     # Force payload to a JS string and headers to JS object
-    print("id_list:", id_list)
+    print("Type of id_list:", type(id_list))
+    print("id_list[:5]:", id_list[:5])
     print("len(id_list):", len(id_list))
     payload = JSON.stringify({"ids": id_list})
     payload_js = String(payload)
